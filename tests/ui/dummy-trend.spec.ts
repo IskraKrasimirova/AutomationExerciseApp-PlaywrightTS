a25f8b39fafeef9a98824dd4e234fc6df534e635
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2026-05-19 23:48:42 +0300

    Update dummy test

--- tests/ui/dummy-trend.spec.ts
+++ tests/ui/dummy-trend.spec.ts
@@ -2,9 +2,14 @@ import { expect, test } from '../setup';
 
 test.describe('Dummy trend ui tests', () => {
 
-    test('Dummy trend ui test', async () => {
+    test('Dummy trend ui test', async ({ pages }) => {
         test.info().annotations.push({ type: "tag", description: "ui" });
-        test.info().annotations.push({ type: "feature", description: "dummy-trend" });  
+        test.info().annotations.push({ type: "feature", description: "dummy-trend" });
+
+        const { homePage } = pages;
+
+        await homePage.open();
+
         expect(true).toBe(true);
     });
 });
