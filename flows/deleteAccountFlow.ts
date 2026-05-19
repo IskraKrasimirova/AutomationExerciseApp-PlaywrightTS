897dddf4b3b4e946bf71d3b330bccc4740f648e3
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2026-05-20 00:05:50 +0300

    Clean gh-pages

--- flows/deleteAccountFlow.ts
+++
@@ -1,8 +0,0 @@
-import { Pages } from '../context/pages';
-
-export async function deleteUserAccount(pages: Pages) {
-    await pages.homePage.navBar.deleteAccount();
-    await pages.accountDeletedPage.verifyAccountDeleted();
-    await pages.accountDeletedPage.clickContinue();
-    await pages.homePage.verifyIsAtHomePage();
-}
--- utils/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  baseUrl: 'https://automationexercise.com/',
-  api: {
-    baseUrl: 'https://automationexercise.com/api/',
-  }
-};
