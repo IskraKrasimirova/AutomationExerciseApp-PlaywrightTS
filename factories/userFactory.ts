124bea6bc5559c7642e23db7e49ec34b57ecdb17
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2026-06-24 00:03:27 +0300

    Fix userFactory to use faker version 7.6.0 properties

--- factories/userFactory.ts
+++ factories/userFactory.ts
@@ -1,18 +1,17 @@
 import { UserModel } from "../models/userModel";
 import { ApiUserModel } from "../models/apiUserModel";
-//import { faker } from "@faker-js/faker";
 const { faker } = require("@faker-js/faker");
-
 
 const titles = ['Mr.', 'Mrs.'];
 const countries = ['India', 'United States', 'Canada', 'Australia', 'Israel', 'New Zealand', 'Singapore'];
 
 export class UserFactory {
+
     static createDefault(): UserModel {
         const birthDate = faker.date.birthdate({ min: 18, max: 60, mode: 'age' });
 
         return {
-            name: faker.person.fullName(),
+            name: faker.name.findName(),
             email: faker.internet.email(),
             password: faker.internet.password(),
             title: faker.helpers.arrayElement(titles),
@@ -22,15 +21,15 @@ export class UserFactory {
             country: faker.helpers.arrayElement(countries),
             subscribeToNewsletter: true,
             receiveSpecialOffers: true,
-            firstName: faker.person.firstName(),
-            lastName: faker.person.lastName(),
-            company: faker.company.name(),
-            address: faker.location.streetAddress(),
-            address2: faker.location.secondaryAddress(),
-            state: faker.location.state(),
-            city: faker.location.city(),
-            zipcode: faker.location.zipCode(),
-            mobileNumber: faker.phone.number()
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            company: faker.company.companyName(),
+            address: faker.address.streetAddress(),
+            address2: faker.address.secondaryAddress(),
+            state: faker.address.state(),
+            city: faker.address.city(),
+            zipcode: faker.address.zipCode(),
+            mobileNumber: faker.phone.phoneNumber()
         };
     }
 
@@ -38,7 +37,7 @@ export class UserFactory {
         const birthDate = faker.date.birthdate({ min: 18, max: 60, mode: 'age' });
 
         return {
-            name: faker.person.fullName(),
+            name: faker.name.findName(),
             email: faker.internet.email(),
             password: faker.internet.password(),
             title: faker.helpers.arrayElement(titles),
@@ -46,27 +45,26 @@ export class UserFactory {
             monthOfBirth: (birthDate.getMonth() + 1).toString().padStart(2, "0"),
             yearOfBirth: birthDate.getFullYear().toString(),
             country: faker.helpers.arrayElement(countries),
-            firstName: faker.person.firstName(),
-            lastName: faker.person.lastName(),
-            company: faker.company.name(),
-            address: faker.location.streetAddress(),
-            address2: faker.location.secondaryAddress(),
-            state: faker.location.state(),
-            city: faker.location.city(),
-            zipcode: faker.location.zipCode(),
-            mobileNumber: faker.phone.number()
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            company: faker.company.companyName(),
+            address: faker.address.streetAddress(),
+            address2: faker.address.secondaryAddress(),
+            state: faker.address.state(),
+            city: faker.address.city(),
+            zipcode: faker.address.zipCode(),
+            mobileNumber: faker.phone.phoneNumber()
         };
     }
 
     static convertUiUserToApiUser(uiModel: UserModel): ApiUserModel {
-        // Convert month name → month number ("January" → "01")
         const monthNumber = new Date(`${uiModel.monthOfBirth} 1, 2000`).getMonth() + 1;
 
         return {
             name: uiModel.name,
             email: uiModel.email,
             password: uiModel.password,
-            title: uiModel.title, 
+            title: uiModel.title,
             dayOfBirth: uiModel.dayOfBirth,
             monthOfBirth: monthNumber.toString(),
             yearOfBirth: uiModel.yearOfBirth,
